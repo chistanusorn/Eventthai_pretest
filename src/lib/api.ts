e7e5b1be9017922b1fedf3f809b32a7111f7ac0a
chistanusorn <specialweek.speciallife@gmail.com>
2026-02-08 12:48:54 +0700

    Switch to local API for login to work

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://reqres.in/api', // <-- Uncomment this line (and comment the one below) to use Real API (VPN/Hotspot required)
-  //baseURL: '/api', // <-- Current: Local Mock (Use this if blocked)
+  // baseURL: 'https://reqres.in/api', // <-- Uncomment this line (and comment the one below) to use Real API (VPN/Hotspot required)
+  baseURL: '/api', // <-- Current: Local Mock (Use this if blocked)
   headers: {
     'Content-Type': 'application/json',
     ...(process.env.NEXT_PUBLIC_REQRES_API_KEY && { 'x-api-key': process.env.NEXT_PUBLIC_REQRES_API_KEY }),
